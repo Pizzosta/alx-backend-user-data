519d0df80fd1e0905b3e141883ae59e19dc1c2a9
pizzosta <pizzostor@gmail.com>
2023-09-04 15:50:20 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-'''Regex-ing Module'''
+"""Definition of filter_datum function that 
+returns an obfuscated log message
+"""
 
 import re
 from typing import List
@@ -7,7 +9,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    '''
+    """
     Returns the log message obfuscated
 
     Args:
@@ -15,6 +17,6 @@ def filter_datum(fields: List[str], redaction: str,
         redaction (str): the value by which the fields will be obfuscated.
         message (str): A str representing the log message.
         separator (str): the character that separates fields in the log msg.
-    '''
+    """
     return re.sub(fr'(?<=^{separator}|{separator})({"|".join(fields)})\
                   (?={separator}|$)', redaction, message)
