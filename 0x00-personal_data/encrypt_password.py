9c4a7e587e77010e826984cc5a11336481add83f
pizzosta <pizzostor@gmail.com>
2023-09-05 11:17:34 +0000

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,17 @@
 
 import bcrypt
 
-def hash_password(password: str)-> bytes:
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt with salt
+
+    Args:
+        password (str): The plain text password to be hashed.
+
+    Returns:
+        bytes: A salted, hashed password as a byte string
+    """
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('UTF-8'), salt)
     return hashed_password
