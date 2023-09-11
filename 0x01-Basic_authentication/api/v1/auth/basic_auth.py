854fec423c724bb54828b223ae117781e201992e
pizzosta <pizzostor@gmail.com>
2023-09-11 16:32:27 +0000

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Definition of class BasicAuth Module """
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Implement Basic Authorization protocol methods
+    """
+    pass
