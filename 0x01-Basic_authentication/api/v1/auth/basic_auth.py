423eae15a9f823581785c2a0425f9c94941822d9
pizzosta <pizzostor@gmail.com>
2023-09-11 18:34:53 +0000

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,7 +22,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
                                            str) -> str:
-        """that returns the decoded value of a Base64 string
+        """returns the decoded value of a Base64 string
         base64_authorization_header
         """
         if base64_authorization_header is None:
@@ -35,3 +35,19 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception:
             None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password
+        from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ':', 1)
+        return user_email, user_password
