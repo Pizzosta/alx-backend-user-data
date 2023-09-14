0a0b2c789e5e7515d96f24167667a9022a6757f1
pizzosta <pizzostor@gmail.com>
2023-09-14 01:39:08 +0100

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,8 @@ class Auth():
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+            if excluded_path.endswith('*') and \
+                    path.startswith(excluded_path[:-1]):
                 return False
             elif excluded_path in {path, path + '/'}:
                 return False
