d053650438c07660d628525750cef07289f8a057
pizzosta <pizzostor@gmail.com>
2023-09-15 21:28:06 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Definition of class BasicAuth Module """
-from typing import TypeVar
-from models.user import User
+""" Definition of class SessionAuth Module """
 from .auth import Auth
 
+
 class SessionAuth(Auth):
+    """Implement Session Authorization protocol methods"""
     pass
