12344e04df502e6e7e7e80718353602be20046f4
pizzosta <pizzostor@gmail.com>
2023-09-19 10:16:08 +0000

    Expiration?

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+""" Handles all routes for the Session Expiry authentication
+"""
+from datetime import datetime, timedelta
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+
+class SessionExpAuth(SessionAuth):
+    def __init__(self):
+        super().__init__()
+        self.session_duration = int(getenv('SESSION_DURATION', 0))
+
+    def create_session(self, user_id=None):
+        session_id = super().create_session(user_id)
+        if session_id:
+            self.user_id_by_session_id[session_id] = {
+                'user_id': user_id,
+                'created_at': datetime.now()
+            }
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        if session_id is None:
+            return None
+
+        session_data = self.user_id_by_session_id.get(session_id)
+
+        if not session_data:
+            return None
+
+        if self.session_duration <= 0:
+            return session_data.get('user_id')
+
+        created_at = session_data.get('created_at')
+
+        if not created_at:
+            return None
+
+        expiration_time = created_at + timedelta(seconds=self.session_duration)
+
+        if expiration_time < datetime.now():
+            return None
+
+        return session_data.get('user_id')
