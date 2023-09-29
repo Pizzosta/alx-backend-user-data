c64ae54b4cf5aef4cd6dafd59e5c95bbdbf5ac8a
pizzosta <pizzostor@gmail.com>
2023-09-29 18:05:21 +0000

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,7 +14,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
